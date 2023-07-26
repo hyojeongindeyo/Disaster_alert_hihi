23fb1e46d7647a30fcdf428bf65c0a28941364a4
ParkDaHyun <pdh23991@gmail.com>
2023-07-26 12:50:26 +0900

    comment

--- boards/urls.py
+++ boards/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('<int:pk>/', views.post_detail, name= 'post_detail'),
     path('create/', views.post_create, name= 'post_create'),
     path('<int:pk>/update/', views.post_update, name= 'post_update'),
+    path('<int:pk>/comment/create/', views.comment_create, name='comment_create'),
+    path('comment/list/', views.comment_list, name='comment_list'),
+
 ]
