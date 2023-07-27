aa2ca75bcd8c892cae08f50a8dd90cfc4a32f88f
KimJiYeon <jsujyeon@naver.com>
2023-07-27 21:11:25 +0900

    makemigrationserror

--- boards/models.py
+++ boards/models.py
@@ -22,7 +22,7 @@ class Board(models.Model) :
     create_date = models.DateTimeField(auto_now=True)  # 피그마에 나와있진 않지만 날짜가 없으면 혼돈 올 듯 해서 넣었슴돠...
     #셀렉트 박스에 넣으려고 초이스로 했어유...
     category_choice = (('서울', '서울'), ('경기도', '경기도'), ('강원도', '강원도'), ('부산', '부산'), ('인천', '인천'), ('대구', '대구'), ('대전', '대전'), ('광주', '광주'),('울산', '울산'), ('세종', '세종'), ('충청북도', '충청북도'), ('충청남도', '충청남도'), ('전라북도', '전라북도'), ('전라남도', '전라남도'),('경상북도', '경상북도'), ('경상남도', '경상남도'), ('제주도', '제주도'))
-    category = models.CharField(max_length=20, choices=category_choice)
+    region = models.CharField(max_length=20, choices=category_choice, null=True)
 
 
 
