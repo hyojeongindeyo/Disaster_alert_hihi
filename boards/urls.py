ab8b3d826fddbf8d876d14d495ee3686d4cf6433
hyojeongindeyo <ghdgywjd510@naver.com>
2023-07-27 23:44:26 +0900

    demo url 연결

--- boards/urls.py
+++ boards/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:pk>/', views.post_detail, name= 'post_detail'),
     path('create/', views.post_create, name= 'post_create'),
     path('<int:pk>/update/', views.post_update, name= 'post_update'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
     path('<int:pk>/comment/create/', views.comment_create, name='comment_create'),
     path('comment/list/', views.comment_list, name='comment_list'),
 
