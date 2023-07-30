e26e8af08ff981c02510b6f19ea50b925e028af0
ParkDaHyun <pdh23991@gmail.com>
2023-07-30 11:58:16 +0900

    main 화면

--- boards/urls.py
+++ boards/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'boards'
 urlpatterns = [
-    path('', views.post_list, name= 'post_list'),
+    path('', views.main_page_view, name='main_page'),
+    path('list/', views.post_list, name= 'post_list'),
     path('<int:pk>/', views.post_detail, name= 'post_detail'),
     path('create/', views.post_create, name= 'post_create'),
     path('<int:pk>/update/', views.post_update, name= 'post_update'),
