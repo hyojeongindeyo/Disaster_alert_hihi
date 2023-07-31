9d267604c7a7a06ba4e41ad77ea727a7c411a916
JHH <johh0588@naver.com>
2023-07-31 16:31:26 +0900

    커뮤니티 페이지-글 상세보기 수정..

--- static/js/communityPost.js
+++ static/js/communityPost.js
@@ -50,12 +50,51 @@ window.addEventListener('click', function(event) {
 });
 
 //신고버튼 활성화 코드
-function appearButton1() {
-    const dynamicButton = document.querySelector('.reportBtn1');
-    dynamicButton.classList.toggle('show');
-};
+// function appearButton1() {
+//     const dynamicButton = document.querySelector('.reportBtn1');
+//     dynamicButton.classList.toggle('show');
+// };
 
-function appearButton2() {
-    const dynamicButton = document.querySelector('.reportBtn2');
-    dynamicButton.classList.toggle('show');
-};
+// function appearButton2() {
+//     const dynamicButton = document.querySelector('.reportBtn2');
+//     dynamicButton.classList.toggle('show');
+// };
+
+// function appearButton(clickedBtn) {
+//     // 클릭된 이미지 버튼의 인덱스 찾기
+//     const imageBtns = document.getElementsByClassName('commentsDots');
+//     const index = Array.from(imageBtns).indexOf(clickedBtn);
+  
+//     // 모든 reportBtn 숨기기
+//     const reportBtns = document.getElementsByClassName('reportBtn');
+//     // for (let i = 0; i < reportBtns.length; i++) {
+//     //   reportBtns[i].style.display = 'none';
+//     // }
+  
+//     // 클릭된 이미지 버튼과 관련된 reportBtn 보이거나 숨기기
+//     const relatedReportBtn = reportBtns[index];
+//     if (relatedReportBtn.style.display === 'none') {
+//       relatedReportBtn.style.display = 'block';
+//     } else {
+//       relatedReportBtn.style.display = 'none';
+//     }
+// }
+
+function appearButton(clickedBtn) {
+    // 클릭된 이미지 버튼의 인덱스 찾기
+    const imageBtns = document.getElementsByClassName('commentsDots');
+    const index = Array.from(imageBtns).indexOf(clickedBtn);
+  
+    // 모든 reportBtn 숨기기
+    const reportBtns = document.getElementsByClassName('reportBtn');
+  
+    // 클릭된 이미지 버튼과 관련된 reportBtn 보이거나 숨기기
+    const relatedReportBtn = reportBtns[index];
+    const computedStyle = window.getComputedStyle(relatedReportBtn);
+    if (computedStyle.display === 'none') {
+      relatedReportBtn.style.display = 'block';
+    } else {
+      relatedReportBtn.style.display = 'none';
+    }
+  }
+  
