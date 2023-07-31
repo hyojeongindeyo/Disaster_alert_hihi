63b86fc6368fd1cfa7e1be8851880da4f1799f02
JHH <johh0588@naver.com>
2023-07-31 17:42:29 +0900

    커뮤니티 페이지-리스트, 글쓰기쪽 css 수정

--- static/js/communityList.js
+++ static/js/communityList.js
@@ -1,14 +1,17 @@
-function appearButton1() {
-    const dynamicButton = document.querySelector('.reportBtn1');
-    dynamicButton.classList.toggle('show');
-}
-
-function appearButton2() {
-    const dynamicButton = document.querySelector('.reportBtn2');
-    dynamicButton.classList.toggle('show');
-}
-
-function appearButton3() {
-    const dynamicButton = document.querySelector('.reportBtn3');
-    dynamicButton.classList.toggle('show');
+function appearButton(clickedBtn) {
+    // 클릭된 이미지 버튼의 인덱스 찾기
+    const imageBtns = document.getElementsByClassName('dotButton');
+    const index = Array.from(imageBtns).indexOf(clickedBtn);
+  
+    // 모든 reportBtn 숨기기
+    const reportBtns = document.getElementsByClassName('reportBtn');
+  
+    // 클릭된 이미지 버튼과 관련된 reportBtn 보이거나 숨기기
+    const relatedReportBtn = reportBtns[index];
+    const computedStyle = window.getComputedStyle(relatedReportBtn);
+    if (computedStyle.display === 'none') {
+      relatedReportBtn.style.display = 'block';
+    } else {
+      relatedReportBtn.style.display = 'none';
+    }
 }
