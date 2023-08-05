d677757790d3e50198f22f15afa9c82b1fa0c5c6
KimJiYeon <jsujyeon@naver.com>
2023-08-06 03:00:12 +0900

    reportbutton&category

--- boards/urls.py
+++ boards/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path
-from . import views
+from .views import *
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.main_page, name='main'),
-    path('board/', views.board_list, name='board_list'),
-    path('board/post/', views.board_post, name='board_post'),
-    path('board/<int:pk>/', views.board_detail, name='board_detail'),
-    path('board/update/<int:pk>/', views.board_update, name='board_update'),
-    path('board/delete/<int:pk>/', views.board_delete, name='board_delete'),
+    path('', main_page, name='main'),
+    path('board/', board_list, name='board_list'),
+    path('board/post/', board_post, name='board_post'),
+    path('board/<int:pk>/', board_detail, name='board_detail'),
+    path('board/update/<int:pk>/', board_update, name='board_update'),
+    path('board/delete/<int:pk>/', board_delete, name='board_delete'),
+    path('board/report/<int:pk>/', board_report, name='board_report'),
 
+    path('comment/post/<int:pk>/', comment_create, name='comment_create'),
+    path('comment/delete/<int:board_id>/<int:comment_id>', comment_delete, name='comment_delete'),
+    path('comment/report/<int:board_id>/<int:comment_id>', comment_report, name='comment_report'),
 
-#    path('board/search/', views.board_search, name='board_search'),
-#    path('board/report/<int:pk>/', views.board_report, name='board_report'),
+    path('region/', region_in_category, name='region_all'),
+    path('region/<category_slug>/', region_in_category, name='region_in_category'),
 
-    path('comment/post/<int:pk>/', views.comment_create, name='comment_create'),
-    path('comment/delete/<int:board_id>/<int:comment_id>', views.comment_delete, name='comment_delete'),
-    # path('<int:pk>/comment/create/', views.comment_create, name='comment_create'),
-    # path('comment/list/', views.comment_list, name='comment_list'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
