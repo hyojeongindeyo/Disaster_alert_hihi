a37850d4327c452ec08537d4c2dd4dc3bd6eb379
JHH <johh0588@naver.com>
2023-08-14 01:36:07 +0900

    지역별 재난문자 즐겨찾기 별 수정

--- static/js/messageMain.js
+++ static/js/messageMain.js
@@ -2,10 +2,12 @@
 function toggleImage(button) {
     const img = button.querySelector("img");
     const src = img.getAttribute("src");
+    let name = src.substring(src.lastIndexOf('/')+1, src.length + 1);
+    console.log(name);
 
-    if (src === "../../../static/image/star_white.png") {
+    if (name === "star_white.png") {
         img.setAttribute("src", "../../../static/image/star_black.png");
-    } else if (src === "../../../static/image/star_black.png") {
+    } else if (name === "star_black.png") {
         img.setAttribute("src", "../../../static/image/star_white.png");
     }
 }
