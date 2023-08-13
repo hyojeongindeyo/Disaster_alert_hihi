a1e0b8e9c390f3d9dadcf43f47712bf64c81c210
JHH <johh0588@naver.com>
2023-08-14 04:25:50 +0900

    지역별 재난문자 폰트 변경 & 미세 조정

--- static/js/communityPost.js
+++ static/js/communityPost.js
@@ -61,11 +61,11 @@ closeModalBtn.addEventListener('click', function() {
     modal.style.display = 'none';
 });
 
-window.addEventListener('click', function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-});
+// window.addEventListener('click', function(event) {
+//     if (event.target === modal) {
+//         modal.style.display = 'none';
+//     }
+// });
 
 //신고버튼 활성화 코드
 // function appearButton1() {
